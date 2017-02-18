b97a65ea5a49e9d4a02229c2c81ce6e5dc05af72
Dusty Wilson <dusty@linux.com>
2017-02-18 11:46:43 +0000

    Attempt to improve reconnection ability; moved to DialTimeout causing API change

--- projector.go
+++ projector.go
@@ -3,10 +3,12 @@ package projector
 import (
 	"bufio"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"log"
 	"net"
 	"sync"
+	"time"
 )
 
 // When using Wireshark, `(data.data contains 05:00:06:00:00:03:00)` works well for grabbing commands
@@ -19,27 +21,29 @@ type Projector struct {
 	Properties    *Properties
 	eventChan     chan Event
 	DebugOutput   bool
+	messageChan   chan []string
 	done          chan struct{}
 	wg            sync.WaitGroup
+	connected     bool
 }
 
+// Errors
+var (
+	ErrNotConnected = errors.New("not connected")
+)
+
 // New creates a new Projector and connects to it
-func New(ip string, eventChan chan Event) (*Projector, error) {
+func New(ip string, eventChan chan Event, connectTimeout time.Duration) (*Projector, error) {
 	p := &Projector{
 		IP:            ip,
 		CommandPrefix: "05000600000300",
 		Properties:    &Properties{},
 		eventChan:     eventChan,
+		messageChan:   make(chan []string),
 		done:          make(chan struct{}),
 	}
-	var err error
-	p.Conn, err = net.Dial("tcp", ip+":41794")
-	if err != nil {
-		return nil, err
-	}
 	go p.listen()
-	p.RefreshProperties()
-	return p, nil
+	return p, p.Connect(connectTimeout)
 }
 
 // Close disconnects from a Projector.
@@ -50,16 +54,34 @@ func (p *Projector) Close() {
 	p.wg.Wait()
 }
 
+// Connect (re)connects the Projector
+// This blocks until it either succeeds or hits the provided expiration
+func (p *Projector) Connect(timeout time.Duration) error {
+	p.wg.Add(1)
+	defer p.wg.Done()
+	if p.connected {
+		return nil
+	}
+	var err error
+	p.Conn, err = net.DialTimeout("tcp", p.IP+":41794", timeout)
+	if err != nil {
+		p.connected = false
+		return err
+	}
+	p.connected = true
+	go p.receiveMessages()
+	p.RefreshProperties()
+	return nil
+}
+
 func (p *Projector) listen() {
 	p.wg.Add(1)
 	defer p.wg.Done()
-	in := make(chan []string)
-	go p.receiveMessages(in)
 	for {
 		select {
 		case <-p.done:
 			return
-		case m := <-in:
+		case m := <-p.messageChan:
 			for i, v := range m {
 				fmt.Printf("IN: [%d] [%s]\n", i, v)
 			}
@@ -67,7 +89,7 @@ func (p *Projector) listen() {
 	}
 }
 
-func (p *Projector) receiveMessages(in chan []string) {
+func (p *Projector) receiveMessages() {
 	p.wg.Add(1)
 	defer p.wg.Done()
 	b := bufio.NewReader(p.Conn)
@@ -79,8 +101,10 @@ func (p *Projector) receiveMessages(in chan []string) {
 			r, err := b.ReadBytes(byte(5))
 			if err != nil {
 				log.Fatal(err)
+				p.connected = false
+				return
 			}
-			in <- p.ParseProperty(r)
+			p.messageChan <- p.ParseProperty(r)
 		}
 	}
 }
@@ -93,6 +117,9 @@ func (p *Projector) RawCommand(h string) error {
 	if err != nil {
 		return err
 	}
+	if p.Conn == nil {
+		return ErrNotConnected
+	}
 	_, err = p.Conn.Write(buf)
 	return err
 }
@@ -102,4 +129,26 @@ func (p *Projector) RawCommand(h string) error {
 // Commands are provided in hex.
 func (p *Projector) Command(h string) error {
 	return p.RawCommand(p.CommandPrefix + h)
+}
+
+// ReconnectIfNeeded will reconnect+retry if the error is a (dis)connection-related error,
+// and will otherwise simply passthorugh the error.
+func (p *Projector) ReconnectIfNeeded(fn func() error) error {
+	err := fn()
+
+	if err == ErrNotConnected { // this is not the only error that should cause a reconnection...  FIXME
+		log.Println("[debug] Reconnecting to projector...")
+		err = p.Connect(time.Second * 5)
+		log.Printf("[debug] Reconnection:[%s]\n", err)
+		if err != nil {
+			return err
+		}
+
+		// This is the retry.  If the connection is still down, it just stays down and returns the error.
+		// If they want it to retry *again*, they can just wrap it however they want...
+		err = fn()
+	}
+
+	log.Printf("[debug] RIN:[%s]\n", err)
+	return err
 }
